b55339262fb794d812e4040c0e9616f334948e4d
letsmailhanh <85898861+letsmailhanh@users.noreply.github.com>
2021-07-20 23:22:09 +0700

    Update SQL 12-7.sql

--- SQL 12-7.sql
+++ SQL 12-7.sql
@@ -171,7 +171,7 @@ INSERT INTO UserInfo(uId, uName,uDOB, uGender, uRole, uPhoneNumber) VALUES (9, '
 INSERT INTO UserInfo(uId, uName,uDOB, uGender, uRole, uPhoneNumber) VALUES (10, 'Mentee Test', '12/12/2001', 1, 'Mentee', '123456789')
 INSERT INTO UserInfo(uId, uName,uDOB, uGender, uRole, uPhoneNumber) VALUES (11, 'Monkey Test', '12/12/2001', 1, 'Mentee', '123456789')
 INSERT INTO UserInfo(uId, uName,uDOB, uGender, uRole, uPhoneNumber) VALUES (12, 'Hoang Minh', '12/12/2001', 1, 'Mentee', '123456789')
-INSERT INTO UserInfo(uId, uName,uDOB, uGender, uRole, uPhoneNumber) VALUES (12, 'Minh Hanh', '12/12/2001', 1, 'Mentor', '123456789')
+INSERT INTO UserInfo(uId, uName,uDOB, uGender, uRole, uPhoneNumber) VALUES (13, 'Minh Hanh', '12/12/2001', 1, 'Mentor', '123456789')
 
 SELECT * From UserInfo
 
